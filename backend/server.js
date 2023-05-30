27a1e13d2e52a8af2b207ce588571cd9bee183b9
irinamihai150 <irinamihai150@gmail.com>
2023-05-30 22:48:37 +0100

    setting backend

---
+++ backend/server.js
@@ -0,0 +1,8 @@
+import express from "express"
+const port = 5000
+const app = express()
+app.get("/", (req, res) => {
+	res.send("api is runing...")
+})
+
+app.listen(port, () => console.log(`Server is running on port ${port}`))
