f0d79a2d2cd492d8390309d316ef685778c90e82
irinamihai150 <irinamihai150@gmail.com>
2023-05-31 20:36:10 +0100

    add files

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,46 @@
 import express from "express"
+import cors from "cors"
+import dotenv from "dotenv"
+import axios from "axios"
+
 const port = 5000
 const app = express()
+app.use(cors())
+app.use(express.json()) // Parse JSON body
+
+dotenv.config()
+
 app.get("/", (req, res) => {
-	res.send("api is runing...")
+	res.send("Api is Running...")
+})
+
+app.post("/api/recommendations", (req, res) => {
+	const apiKey = process.env.OPENAI_API_KEY
+	const apiUrl = "https://api.openai.com/v1/completions"
+	const prompt = `Please suggest movies based on specific genres, plot elements, or any preferences you think would suit my taste: ${req.body.inputText}`
+
+	axios
+		.post(
+			apiUrl,
+			{
+				model: "text-davinci-003",
+				prompt: prompt,
+				max_tokens: 100,
+			},
+			{
+				headers: {
+					"Content-Type": "application/json",
+					Authorization: `Bearer ${apiKey}`,
+				},
+			}
+		)
+		.then((response) => {
+			res.json(response.data)
+		})
+		.catch((error) => {
+			console.error(error)
+			res.status(500).json({ error: "An error occurred" })
+		})
 })
 
 app.listen(port, () => console.log(`Server is running on port ${port}`))
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,82 @@
-import React from 'react'
-
+import { useState } from "react"
+import axios from "axios"
+import { Card, Button } from "react-bootstrap"
 const App = () => {
-  return (
-    <div>
-      <h2>hello</h2>
-    </div>
-  )
+	const [inputText, setInputText] = useState("")
+	const [responseText, setResponseText] = useState("")
+	const handleButtonClick = () => {
+		const apiUrl = "http://localhost:5000/api/recommendations"
+
+		const prompt = `Please suggest movies based on specific genres, plot elements, or any preferences you think would suit my taste:${inputText}`
+		const payload = {
+			model: "text-davinci-003",
+			prompt: `What are some movie recommendations you can provide? Please suggest movies based on specific genres, plot elements, or any preferences you think would suit my taste: ${inputText}`,
+			max_tokens: 100,
+		}
+
+		axios
+			.post(apiUrl, payload, {
+				headers: {
+					"Content-Type": "application/json",
+				},
+			})
+
+			.then((response) => {
+				setResponseText(response.data.choices[0].text)
+				console.log(response.data)
+			})
+			.catch((error) => {
+				console.error(error.response)
+			})
+	}
+
+	return (
+		<div className='app'>
+			<Card style={{ textAlign: "center", margin: "3em" }}>
+				<Card.Body className='gradient-background'>
+					<div>
+						<Card.Title>
+							Give me one sentence and I will give you a movie title!
+						</Card.Title>
+						<Card.Img
+							style={{ height: "10em", width: "10em", margin: "1em" }}
+							src='./images/robot.jpg'
+							alt='robot'
+						></Card.Img>
+					</div>
+					<div
+						style={{
+							textAlign: "center",
+							margin: "auto",
+							display: "flex",
+							flexDirection: "column",
+						}}
+					>
+						<textarea
+							value={inputText}
+							onChange={(e) => setInputText(e.target.value)}
+							cols='20'
+							rows='5'
+						></textarea>
+						<Button
+							type='submit'
+							className='submit-button'
+							onClick={handleButtonClick}
+							style={{ padding: "2em", margin: "1em" }}
+						>
+							<i className='fas fa-arrow-right'></i>
+						</Button>
+					</div>
+					{responseText && (
+						<div>
+							<h2>Suggestion:</h2>
+							<p>{responseText}</p>
+						</div>
+					)}
+				</Card.Body>
+			</Card>
+		</div>
+	)
 }
 
 export default App
