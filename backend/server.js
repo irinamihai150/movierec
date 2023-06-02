12c8a06b8e927bf6e51b9ce5f5147ca1838ad663
irinamihai150 <irinamihai150@gmail.com>
2023-06-02 20:39:51 +0100

    fix server

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ import express from "express"
 import cors from "cors"
 import dotenv from "dotenv"
 import axios from "axios"
+import path from "path"
+
 
 const port = 5000
 const app = express()
@@ -10,9 +12,21 @@ app.use(express.json()) // Parse JSON body
 
 dotenv.config()
 
+// app.get("/", (req, res) => {
+// 	res.send("Api is Running...")
+// })
+
+// Get the directory path using __dirname
+const __dirname = path.resolve();
+
+// Serve the React build files as static assets
+app.use(express.static(path.join(__dirname, "frontend/build")));
+
 app.get("/", (req, res) => {
-	res.send("Api is Running...")
-})
+  res.sendFile(path.join(__dirname, "frontend/build", "index.html"));
+});
+
+
 
 app.post("/api/recommendations", (req, res) => {
 	const apiKey = process.env.OPENAI_API_KEY
